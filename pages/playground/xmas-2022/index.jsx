ee00a12d79013b679de64f8bc79f25c8bc7031b0
Antoine Macia <antoinemacia@dev.local>
2022-12-18 00:22:45 +1100

    add to blog

---
+++ pages/playground/xmas-2022/index.jsx
@@ -0,0 +1,13 @@
+import dynamic from 'next/dynamic'
+
+const Model = dynamic(() => import('./Model'), { ssr: false })
+
+export default function Page() {
+  return <Model />
+}
+
+import LayoutWrapper from '@/components/LayoutWrapper'
+
+Page.getLayout = function getLayout(page) {
+  return <LayoutWrapper>{page}</LayoutWrapper>
+}
