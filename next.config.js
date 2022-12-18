e9e3c7b245a4b6a020fd337dddd63a0b491b5353
Antoine Macia <antoinemacia@dev.local>
2022-12-18 11:18:04 +1100

    Remove normal map

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+const withTM = require('next-transpile-modules')(['three'])
+
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
@@ -57,56 +59,58 @@ const securityHeaders = [
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
-module.exports = withContentlayer(
-  withBundleAnalyzer({
-    reactStrictMode: true,
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-    eslint: {
-      dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
-    },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: securityHeaders,
-        },
-      ]
-    },
-    // async rewrites() {
-    //   return [
-    //     {
-    //       source: '/xmas-2022',
-    //       destination: '/xmas-2022/index.html',
-    //     },
-    //   ]
-    // },
-    webpack: (config, { dev, isServer }) => {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      })
-      config.module.rules.push({
-        test: /\.(glb|gltf)$/,
-        use: {
-          loader: 'file-loader',
-          // options: {
-          //   publicPath: "/_next/static/images",
-          //   outputPath: "static/images/",
-          // }
-        },
-      })
-
-      if (!dev && !isServer) {
-        // Replace React with Preact only in client production build
-        Object.assign(config.resolve.alias, {
-          'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-          react: 'preact/compat',
-          'react-dom/test-utils': 'preact/test-utils',
-          'react-dom': 'preact/compat',
+module.exports = withTM(
+  withContentlayer(
+    withBundleAnalyzer({
+      reactStrictMode: true,
+      pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+      eslint: {
+        dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
+      },
+      async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: securityHeaders,
+          },
+        ]
+      },
+      // async rewrites() {
+      //   return [
+      //     {
+      //       source: '/xmas-2022',
+      //       destination: '/xmas-2022/index.html',
+      //     },
+      //   ]
+      // },
+      webpack: (config, { dev, isServer }) => {
+        config.module.rules.push({
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
         })
-      }
+        config.module.rules.push({
+          test: /\.(glb|gltf)$/,
+          use: {
+            loader: 'file-loader',
+            // options: {
+            //   publicPath: "/_next/static/images",
+            //   outputPath: "static/images/",
+            // }
+          },
+        })
 
-      return config
-    },
-  })
+        if (!dev && !isServer) {
+          // Replace React with Preact only in client production build
+          Object.assign(config.resolve.alias, {
+            'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+            react: 'preact/compat',
+            'react-dom/test-utils': 'preact/test-utils',
+            'react-dom': 'preact/compat',
+          })
+        }
+
+        return config
+      },
+    })
+  )
 )
